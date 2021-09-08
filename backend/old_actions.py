b0b9a96e1ccec1eb0eca45e0ef62009e9dfce4d5
miksuk28 <74315076+miksuk28@users.noreply.github.com>
2021-09-08 22:26:14 +0200

    rename
    renamed actions.py => old_actions.py

--- backend/old_actions.py
+++ backend/old_actions.py
@@ -1,3 +1,3 @@
 def create_user(fname, lname, username, password):
     ''' Adds the user to the database, hashes password '''
-    print("User should be created" + a)
+    print("User should be created")
